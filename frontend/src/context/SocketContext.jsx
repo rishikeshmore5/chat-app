74aa483cbdbbae6bbacb541169b71cd1ad8739c6
rishikeshmore5 <rishikeshmore5@gmail.com>
2024-02-05 22:58:58 +0530

    Socket

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,8 +15,8 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      // const socket = io("https://chat-app-yt.onrender.com", {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://chat-app-s9ox.onrender.com/", {
+      // const socket = io("http://localhost:5000", {
         query: {
           userId: authUser._id,
         },
